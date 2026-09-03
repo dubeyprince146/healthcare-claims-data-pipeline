86b98bd94e81956a76dcb2a87bc0b2d0884332f5
dubeyprince146 <dubey.prince146@gmail.com>
2026-09-03 11:15:17 +0900

    daily summary and clean data reconciliation

--- sql/snowflake/m6_transformations.sql
+++ sql/snowflake/m6_transformations.sql
@@ -104,3 +104,64 @@ as
     ) as invalid_status_count
 from
     claims_clean;
+
+--Grain-> service_date would be the grain of this claim summary where all the calculation would be
+--done for each service date.
+--Measures-> Six required measures value would be
+                --Total claim count
+                --Total claim amount
+                -- Approved claim count
+                -- Denied claim count
+                --Pending claim count
+                --Approved claim amount
+-- Consumer and decision-> This will be required for business decisions where a claims manager can identify days with unusually high denied or pending claim volumes.
+
+create or replace table curated.DAILY_CLAIM_SUMMARY
+as
+select
+service_date,
+count(*) as total_claim_count,
+sum(claim_amount) as total_claim_amount,
+count_if(claim_status = 'APPROVED') as approved_claim_count,
+count_if(claim_status = 'DENIED') as denied_claim_count,
+count_if(claim_status = 'PENDING') as pending_claim_count,
+sum(iff(claim_status = 'APPROVED',claim_amount,0)) as approved_claim_amount
+from claims_clean
+group by service_date
+order by service_date;
+
+select *
+from daily_claim_summary
+order by service_date;
+
+--Data reconciliation between daily summary and clean rows
+
+with daily_summary as
+(
+    select
+        count(*) as summary_row_count,
+        sum(total_claim_count) as total_claim_count_sum,
+        sum(total_claim_amount) as total_claim_amount,
+        sum(APPROVED_CLAIM_COUNT+DENIED_CLAIM_COUNT+PENDING_CLAIM_COUNT) as summed_status_count
+    from daily_claim_summary
+),
+
+claims_clean_summary as
+(
+    select
+        count(distinct service_date) as total_clean_date_count,
+        count(*) as total_clean_row_count,
+        sum(claim_amount) as total_clean_claim_amounts
+    from claims_clean
+)
+
+select
+    s.summary_row_count,
+    c.total_clean_date_count,
+    s.total_claim_count_sum,
+    c.total_clean_row_count as clean_count_for_claim_reconciliation,
+    s.total_claim_amount,
+    c.total_clean_claim_amounts,
+    s.summed_status_count,
+    c.total_clean_row_count as clean_count_for_status_reconciliation
+from daily_summary s,claims_clean_summary c;
